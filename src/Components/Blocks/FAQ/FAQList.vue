f86b716f291e51f439d15f1d304b5a36df298879
ahmad <doregard4@gmail.com>
2023-02-26 12:34:08 +0330

    refactor: FAQ block

--- src/Components/Blocks/FAQ/FAQList.vue
+++ src/Components/Blocks/FAQ/FAQList.vue
@@ -1,25 +1,25 @@
 <template>
-    <div :class="{'bg-gray-900 text-gray-100': block.data.darkMode, 'bg-white': !block.data.darkMode}">
+    <section :style="{ backgroundColor: block.data.blockBackgroundColor }" class="py-5">
         <div class="max-w-6xl mx-auto">
-            <div :class="{'py-6 md:pt-12 pb-24': first, 'py-12 md:py-24': !first}" class="px-4 md:px-8">
+            <div class="py-6 md:pt-12 pb-24 px-4 md:px-8">
                 <div class="space-y-12 text-center">
-                    <h1 class="max-w-3xl text-3xl md:text-4xl font-bold mx-auto">
+                    <h4 class="max-w-3xl text-2xl md:text-3x font-bold mx-auto">
                         {{ block.data.title }}
-                    </h1>
+                    </h4>
                 </div>
                 <div class="max-w-3xl mx-auto mt-16 space-y-8">
                     <div v-for="faq in block.data.list" :key="faq.uuid" class="">
-                        <div class="text-xl md:text-2xl font-normal">
+                        <div class="md:text-xl font-normal">
                             {{ faq.question }}
                         </div>
-                        <div :class="{'text-gray-300': block.data.darkMode, 'text-gray-600': !block.data.darkMode}" class="text-gray-600 text-base md:text-lg font-light">
+                        <div class="text-base font-light">
                             <p>{{ faq.answer }}</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 <script>
